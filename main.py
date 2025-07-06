21d372ed9fc83a5a58f4b2f6a662252d5f23c0a2
aubejay22 <aubejay22@gmail.com>
2025-07-06 17:09:20 -0400

    Replace old project with new version

---
+++ main.py
@@ -0,0 +1,137 @@
+import argparse
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+import os
+from prafa.portfolio import Portfolio
+from prafa.universe import Universe
+
+
+def Main():
+    # Set the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, 
+                    default='financial_data')
+
+    parser.add_argument('--result_path', type=str, 
+                    default='results') # default=os.getcwd()+'/results'
+    parser.add_argument('--solution_name', type=str,
+                    default='lagrange_full')#,] choices=['lagrange_full', 'lagrange_ours',  'lagrange_forward', 'lagrange_backward'])
+
+    parser.add_argument('--cardinality', type=int, default=30)
+
+    # Select the Data to Use
+    parser.add_argument('--start_date', type=str, default="2014-01-02")
+    parser.add_argument('--end_date', type=str, default="2025-01-02")
+    parser.add_argument('--index', type=str,
+                    default='sp500')#, choice=['sp500', 'russel, nikkei])
+
+
+    #nombre de jours 
+    parser.add_argument('--T', type=int, default=2, help="nombre d'année pour l'entrainement")
+    parser.add_argument('--rebalancing', type=int, default=6, help="Month increment for rebalancing")
+    args = parser.parse_args()
+    
+  
+
+    
+    #fenetre d'entrainement
+    portfolio_duration = relativedelta(years=args.T)
+
+    #pour le rebalancement
+    time_increment = relativedelta(months=args.rebalancing)
+
+    #liste des dates de rebalancement
+    start_date = pd.to_datetime(args.start_date)
+    end_date = pd.to_datetime(args.end_date)
+    
+    # Construire la liste des dates
+    dates = [start_date]
+    current_date = start_date + time_increment
+
+    while current_date < end_date:
+        dates.append(current_date)
+        current_date += time_increment
+    
+    #initialisation des object necessaire pour extraire les portefeuilles dans le temps
+    portfolio = Portfolio(Universe(args))
+    for rebalancing_date in dates:
+        start_datetime = rebalancing_date - portfolio_duration
+        portfolio.rebalance_portfolio(start_datetime, rebalancing_date)
+        print(f"Rebalancing from {start_datetime.date()} to {rebalancing_date.date()}")
+
+    
+    return None
+
+"""
+if __name__ == "__main__":
+    Main()
+"""
+
+import pickle
+from argparse import Namespace
+import matplotlib.pyplot as plt
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+from datetime import datetime
+
+
+filepath = 'results/portfolio_sp500_lagrange_full_30.json'
+with open(filepath, 'rb') as f:
+    portfolios = pickle.load(f)
+
+
+args = Namespace()
+args.index = 'sp500'
+args.data_path = 'financial_data'
+args.result_path = 'results'
+args.solution_name = 'lagrange_full'
+args.rebalancing = 6  
+args.start_date = '2014-01-02'
+args.end_date = '2025-01-02'
+
+# Initialiser l'objet Universe
+universe = Universe(args)
+
+
+# S'assurer que les dates sont ordonnées
+dates = sorted(list(portfolios.keys()))
+n = len(dates)
+
+rendements_portefeuille = []
+rendements_indice = []
+index_dates = []
+
+for i in range(n - 1):  # On va jusqu'à n-1 car on a besoin de t et t+1
+    start_date = dates[i]
+    end_date = dates[i + 1] - pd.tseries.offsets.BDay(1)  # veille du prochain rebalance
+
+    # Extraire rendements entre deux rebalancements
+    universe.new_universe(start_date, end_date, training=False)
+    window_returns = universe.get_stocks_returns()
+    weights = list(portfolios[start_date].values())
+
+    # Produit matriciel : rendements du portefeuille
+    rp = (window_returns @ weights)
+    ri = universe.get_index_returns()
+
+    rendements_portefeuille += rp
+    rendements_indice += ri
+    index_dates += list(window_returns.index)
+
+# Construire les séries temporelles
+rendements_portefeuille = pd.Series(rendements_portefeuille, index=index_dates)
+rendements_indice = pd.Series(rendements_indice, index=index_dates)
+
+# Cumuler les rendements
+rendements_cumules_portefeuille = (rendements_portefeuille + 1).cumprod() -1
+rendements_cumules_indice = (rendements_indice + 1).cumprod() - 1
+
+# Tracer
+ax = rendements_cumules_portefeuille.plot(label="Portefeuille")
+rendements_cumules_indice.plot(ax=ax, label="Indice de référence", linestyle='--')
+ax.set_title("Performance cumulée (hors-échantillon)")
+ax.set_ylabel("Rendement cumulé")
+ax.legend()
+plt.grid(True)
+plt.show()
---
+++ prafa/universe.py
@@ -0,0 +1,162 @@
+import pandas as pd
+from datetime import datetime
+import numpy as np
+
+#_________________________________________________________________
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#             va me rester a traiter les Nan dans new_universe!!!!
+
+#-----------------------------------------------------------------
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+class Universe():
+    
+    
+    def __init__(
+        self,
+        args,
+    ) :
+        self.args = args
+        
+        #données sur toutes l'historique
+        self.initialisation_donnes()
+
+        #timeseries sur la periode en cours
+        self.df_return = None
+        self.df_index = None
+
+        self.stock_list = self.update_stock_list(None)
+    
+
+    def initialisation_donnes(self):
+        #données sur toutes l'historique
+        self.df_return_all = pd.read_csv(f"financial_data/{self.args.index}/returns_stocks.csv")  #return des stocks 
+        self.df_return_all.columns = [col.split()[0].replace('/', '.') for col in self.df_return_all.columns]
+        self.df_return_all.index = pd.to_datetime(self.df_return_all.index)
+        
+        self.df_index_all = pd.read_csv(f"financial_data/{self.args.index}/returns_index.csv")   #return de l'indice
+
+        #self.df_return_all['Date'] = pd.to_datetime(self.df_return_all['Date'])
+        #self.df_return_all.set_index('Date', inplace=True)
+        
+        self.df_index_all['Date'] = pd.to_datetime(self.df_index_all['Date'])
+        self.df_index_all.set_index('Date', inplace=True)
+
+
+    
+    def update_stock_list(self, datetime : datetime = None):
+        #ce code va aller chercher la compositon
+        #df = lambda year : pd.read_csv(f"financial_data/{self.args.index}/constituants/{year}.csv",usecols=["Ticker"]).str.split().str[0].str.replace("/", ".")
+        df = lambda year: pd.read_csv(
+                f"financial_data/{self.args.index}/constituants/{year}.csv", 
+                usecols=["Ticker"]
+            )["Ticker"].str.split().str[0].str.replace("/", ".")
+
+        if datetime is None:
+            #appelle dans le constructeur premier universe
+            self.year = int(self.args.start_date[0:4])
+            self.stock_list = df(self.year).tolist()
+
+        elif datetime.year != self.year:
+            #puisque le rebalancement se fait par an,
+            #on va chercher la liste des stocks pour l'année en cours
+            #sinon on va chercher les stocks pour la nouvelle année
+            self.year = datetime.year
+            self.stock_list = df(self.year).tolist()
+
+        return self.stock_list
+    
+
+    def new_universe(
+        self,
+        start_datetime : datetime,
+        end_datetime : datetime,
+        training : bool = True
+    )   :
+        """
+            Create a new universe with the specified time range.
+
+            par contre, dependamment si l'univers est pour entrainement ou pour le backtesting, on va devoir changer 
+            ou on appelle la fonction get_stock_list
+            si c'est pour l'entrainement, on va chercher la liste des stocks au moment end_datetime
+            si c'est pour le backtesting, on va chercher la liste des stocks au moment start
+        """
+      
+        
+        if type(start_datetime) != type(pd.Timestamp('now')):
+            start_datetime = pd.Timestamp(start_datetime)
+        if type(end_datetime) != type(pd.Timestamp('now')):
+            end_datetime = pd.Timestamp(end_datetime)
+        
+        #ajustement des stocks dans l'univers
+        if training:
+            self.update_stock_list(end_datetime)
+        else:
+            self.update_stock_list(start_datetime)
+        
+        # ⚠️ À mettre dans la méthode new_universe juste avant d'extraire les rendements :
+        valid_stocks = [stock for stock in self.stock_list if stock in self.df_return_all.columns]
+        missing_stocks = set(self.stock_list) - set(valid_stocks)
+
+        if missing_stocks:
+            print(f"⚠️ Les actions suivantes ne sont pas dans les données de rendement : {missing_stocks}")
+        self.stock_list = valid_stocks
+        
+        print(f"Stock list used: {self.stock_list}")
+        print(f"Window: {start_datetime} to {end_datetime}")
+        print(f"Available stock columns: {self.df_return_all.columns.tolist()[:5]} ...")
+        print(f"Shape of extracted returns: {self.df_return.shape}")
+        print(f"Shape of extracted index returns: {self.df_index.shape}")
+
+        #retourne les stocks de l'univers au bonne periode de temps
+        stocks_returns = self.df_return_all.loc[start_datetime:end_datetime, self.stock_list]
+        index_returns = self.df_index_all.loc[start_datetime:end_datetime]
+        common_index = stocks_returns.index.intersection(index_returns.index)
+        
+        self.df_return = stocks_returns.loc[common_index]
+        self.df_index = index_returns.loc[common_index]
+        self.data_cleaning()
+
+
+    def data_cleaning(self):
+        # Nombre de colonnes avant suppression
+        colonnes_avant = self.df_return.shape[1]
+
+        # Supprimer les colonnes avec plus de 10 NaN
+        self.df_return = self.df_return.loc[:, self.df_return.isna().sum() <= 10]
+        self.stock_list = self.df_return.columns.to_list()
+
+        # Nombre de colonnes après suppression
+        colonnes_apres = self.df_return.shape[1]
+        print(f"Removed {colonnes_avant - colonnes_apres} columns due to too many missing values.")
+        
+        # Supprimer les lignes avec au moins un NaN
+        lignes_avant = self.df_return.shape[0]
+        self.df_return.dropna(inplace=True)
+        lignes_apres = self.df_return.shape[0]
+        print(f"Removed {lignes_avant - lignes_apres} rows due to missing values.")
+        
+        self.df_index.dropna(inplace=True)
+
+        common_index = self.df_return.index.intersection(self.df_index.index)
+        
+        self.df_return = self.df_return.loc[common_index]
+        self.df_index = self.df_index.loc[common_index]
+
+
+
+    def get_stocks_returns(self):
+        return self.df_return
+    
+    def get_index_returns(self):
+        return self.df_index
+    
+    def get_stock_list(self):
+        return self.stock_list
+    
+    def get_number_of_stocks(self):
+        return len(self.stock_list)
+    
